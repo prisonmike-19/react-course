a652d4b63fac932865a7aa2874c7a00f181077f8
prisonmike-19 <aditisharma112227@gmail.com>
2025-12-08 17:35:30 +0530

    ne commit

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 // const heading=React.createElement("h1",{id:"heading"},"Hello World from React");
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 
 const parent = React.createElement(
   "div",
   { id: "parent" },
   [React.createElement(
     "div",
-    { id: "child" },
-    [React.createElement("h1", { id: "heading" }, "Nested Heading "),React.createElement("h2", { id: "heading2" }, "Nested Heading 2")]
+    { id: "child", key:"child-1" },
+    [React.createElement("h1", { id: "heading", key: "h1-1" }, "Nested Heading "),React.createElement("h2", { id: "heading2", key:"h1-2" }, "Nested Heading 2")]
   ), React.createElement(
     "div",
-    { id: "child2" },
-    [React.createElement("h1", { id: "heading" }, "Nested Heading child 2"),React.createElement("h2", { id: "heading2" }, "Nested Heading 2 child 2")]
+    { id: "child2" , key:"child-2" },
+    [React.createElement("h1", { id: "heading" ,key:"h2-1"}, "Nested Heading child 2"),React.createElement("h2", { id: "heading2" ,key:"h2-2"}, "Nested Heading 2 child 2")]
   )]
 );
 
